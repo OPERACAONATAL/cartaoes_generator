a37148dbc0522865fce332ffcd1c2c590d9fcc03
Fazendaaa <lucas.carotta@outlook.com>
2017-09-20 11:55:10 -0300

    Corrigindo estética.

--- generator.py
+++ generator.py
@@ -14,16 +14,26 @@ from tqdm import tqdm
 from PIL import Image
 from pdfrw import PdfReader, PdfWriter
 import os
+import math
+import re
+import unidecode
 
 background = cv2.imread('./dist/img/background.png')
 color = 0
 font = cv2.FONT_HERSHEY_TRIPLEX
 scale = 1
-offset = 0
+x_offset = 25
+y_offset = 5
 thick = 2
+crop = 30
+elders = ['Cantinho Fraterno', 'Helena Dornfeld']
 
 df = pd.read_csv('./src/data/data.csv')
 length = df.shape[0]
+# length = 15
+
+def removeAccent(src):
+    return unidecode.unidecode(str(src))
 
 def insertData(id, data):
     # Sei que isso é feio, mas não sei como salvar o qr em um buffer pro opencv ler
@@ -32,20 +42,38 @@ def insertData(id, data):
     
     qrcode = cv2.imread(f"./dist/img/qrcode/id-{id}.png")
     
+    # Corte as bordas em branco do qrcode
+    qrcode = qrcode[crop: qrcode.shape[0], crop: qrcode.shape[1]]
+
     new_background = cp.deepcopy(background)
-    new_background[0:offset + qrcode.shape[0], 0:offset + qrcode.shape[1]] = qrcode
+    new_background[y_offset:qrcode.shape[0] + y_offset, x_offset:qrcode.shape[1] + x_offset] = qrcode
 
     # Adiciona as informações no cartão
-    cv2.putText(new_background, f"{data['Nome da Instituição']}", (440, 140), font, 2, color, thickness=4)
+    institution = removeAccent(data['Nome da Instituição'])
+    # 'Oito' é um constante encontrada empiricamente para converter em uma escala o tamanho da letra
+    middle = int(len(institution)/2)*8
+    cv2.putText(new_background, institution, (600-middle, 140), font, scale, color, thickness=thick)
 
-    cv2.putText(new_background, f"Nome: {data['Nome do Assistido']}", (280, 220), font, scale, color, thickness=thick)
-    cv2.putText(new_background, f"Sexo: {data['Sexo']}", (280, 260), font, scale, color, thickness=thick)
-    cv2.putText(new_background, f"ID: {id}", (670, 260), font, scale, color, thickness=thick)
-    cv2.putText(new_background, f"Idade: {data['Idade']}", (950, 260), font, scale, color, thickness=thick)
+    cv2.putText(new_background, removeAccent(data['Nome do Assistido']), (400, 230), font, scale, color, thickness=thick)
+    cv2.putText(new_background, removeAccent(data['Sexo']), (380, 260), font, scale, color, thickness=thick)
+    cv2.putText(new_background, f"ID:{id}", (30, 280), font, scale+1, color, thickness=thick)
+    cv2.putText(new_background, removeAccent(data['Idade']), (1040, 260), font, scale, color, thickness=thick)
 
-    cv2.putText(new_background, f"Calca: {data['Nº Calça']}", (30, 365), font, scale, color, thickness=thick)
-    cv2.putText(new_background, f"Camista: {data['Nº Camiseta']}", (30, 395), font, scale, color, thickness=thick)
-    cv2.putText(new_background, f"Sapato: {data['Nº Calçado']}", (30, 425), font, scale, color, thickness=thick)
+    cv2.putText(new_background, removeAccent(data['Nº Calça']), (130, 360), font, scale, color, thickness=thick)
+    cv2.putText(new_background, removeAccent(data['Nº Camiseta']), (180, 390), font, scale, color, thickness=thick)
+    cv2.putText(new_background, removeAccent(data['Nº Calçado']), (150, 425), font, scale, color, thickness=thick)
+
+    if data['Nome da Instituição'] in elders:
+         cv2.putText(new_background, "Fralda geriatrica", (30, 450), font, scale, color, thickness=thick)
+    # Se tiver texto na idade, significa que tem "meses" -- ou seja, bebês
+    elif isinstance(data['Idade'], str):
+        cv2.putText(new_background, "Brinquedo", (30, 455), font, scale, color, thickness=thick)
+    # Tem que verificar porque NaN é um tipo de float e ao mesmo tempo é o dado atribuido ao campos sem valores em uma csv
+    elif not math.isnan(data['Idade']) and data['Idade'] < 12:
+        cv2.putText(new_background, "Brinquedo", (30, 455), font, scale, color, thickness=thick)
+    # Caso não seja idoso, ou criança, mas sim um adolescente -- ou a idade é um atributo vazio
+    else:
+        cv2.putText(new_background, "Livro", (30, 455), font, scale, color, thickness=thick)
 
     # Salva a imagem do cartão
     cv2.imwrite(f"./dist/img/card/OPN-{i}.png", new_background)
